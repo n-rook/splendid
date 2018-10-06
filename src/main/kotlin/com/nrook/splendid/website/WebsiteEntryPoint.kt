0fb85b9239a0fa1db2d99ddbe55c796da13a544e
Nathaniel Rook <nrook@nrook.com>
2018-10-06 06:37:05 -0700

    Basic Web API entry point

---
+++ src/main/kotlin/com/nrook/splendid/website/WebsiteEntryPoint.kt
@@ -0,0 +1,20 @@
+package com.nrook.splendid.website
+
+import io.ktor.application.call
+import io.ktor.http.ContentType
+import io.ktor.response.respondText
+import io.ktor.routing.get
+import io.ktor.routing.routing
+import io.ktor.server.netty.Netty
+
+fun main(args: Array<String>) {
+  val server = io.ktor.server.engine.embeddedServer(Netty, 8080) {
+    routing {
+      get("/") {
+        call.respondText("Hello, world!", ContentType.Text.Plain)
+      }
+    }
+  }
+
+  server.start(true)
+}
