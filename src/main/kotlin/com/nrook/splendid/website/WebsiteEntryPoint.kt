3f2955c952f0ec7faee5187f490af443040f0893
Nathaniel Rook <nrook@nrook.com>
2018-11-16 13:44:24 -0800

    Add comments describing hypothetical web API

--- src/main/kotlin/com/nrook/splendid/website/WebsiteEntryPoint.kt
+++ src/main/kotlin/com/nrook/splendid/website/WebsiteEntryPoint.kt
@@ -18,3 +18,16 @@ fun main(args: Array<String>) {
 
   server.start(true)
 }
+
+// API design
+// Game API:
+// POST /game/start: Start new game. Returns Game object with URL and ID
+// GET /game/123: Get game data.
+// GET /game/123/moves: Get available moves.
+// GET /game/123/0, /game/123/0x, etc: Get historical data on previous turn.
+// POST /game/123/move: Make a move. Returns updated game info.
+
+// DATA TYPES:
+// "game"
+// "move"
+//
